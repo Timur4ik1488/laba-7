0e527b9eb67fb2dd1a468c5d337c537637a53d40
Timur <kirichenkotimur@gmail.com>
2024-11-20 21:37:05 +0300

    Конечное задание 2
    Сделал проверку вводимых данных и проверку на ненулевые значения, всё работает

--- Задание 2.py
+++ Задание 2.py
@@ -1,20 +1,23 @@
 from random import randint
 
-n = int(input("Введите количество строк и столбцов: "))
-auto_list = [[[randint(0,20) for _ in range(n)] for _ in range(n)]]
-auto_list1 = [[[randint(0,20) for _ in range(n)] for _ in range(n)]]
-print("Первый список: ", *auto_list, sep = "\n")
-print("Второй список: ", *auto_list1, sep = "\n")
+try:
+    n = int(input("Введите количество строк и столбцов: "))
+    if n > 0:
+        auto_list = [[[randint(0,20) for _ in range(n)] for _ in range(n)]]
+        auto_list1 = [[[randint(0,20) for _ in range(n)] for _ in range(n)]]
+        print("Первый список: ", *auto_list, sep = "\n")
+        print("Второй список: ", *auto_list1, sep = "\n")
 
-l = len(auto_list)
-z = []
-for i in range(l): # цикл проходит по строкам
-    z = [*z, [] ]
-    for j in range(len(auto_list[i])): # цикл проходит по элементам в строке
-        z [i] = [*z[i], auto_list [i][j]]
-        z [i] = [*z[i], auto_list1 [i][j]]
-print("Список после преобразований: ", *z, sep = "\n")
-
-
-
-
+        l = len(auto_list)
+        z = []
+        for i in range(l): # цикл проходит по строкам
+            z = [*z, [] ]
+            for j in range(len(auto_list[i])): # цикл проходит по элементам в строке
+                z [i] = [*z[i], auto_list [i][j]]
+                z [i] = [*z[i], auto_list1 [i][j]]
+        print("Список после преобразований: ", *z, sep = "\n")
+    else:
+        print("Введите ненулевое значение")
+except:
+    print("Неверно введены значения")
+        
