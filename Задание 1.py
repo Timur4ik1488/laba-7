79f23b24dfdaea6217967fd48209e6fb31b39210
Timur <kirichenkotimur@gmail.com>
2024-11-20 19:16:21 +0300

    Задание 1
    Код рабочий, всё хорошо, проверка на вводимые данные есть

---
+++ Задание 1.py
@@ -0,0 +1,18 @@
+try:
+    from random import randint
+
+    m=int(input("Введите количество строк: "))
+    n=int(input("Введите значение элементов в строках: "))
+    auto_list = [[randint(0,20) for _ in range(n)] for _ in range(m)]
+    print("Исходный список: ", *auto_list, sep="\n")
+
+    for i in range(len(auto_list)):
+        if i%2!=0:
+            auto_list[i].reverse()
+    print("Выходной список: ", *auto_list, sep = "\n")
+
+except:
+    print("Неверно введены значения для количества строк и элементов")
+
+
+
