80eb9bb0490681cbe6b34de6cb338d01c20e58b7
Timur <kirichenkotimur@gmail.com>
2024-11-20 19:36:33 +0300

    Изменения в задание 1
    Был убран метод reverse, теперь переворот осуществляется срезом

--- Задание 1.py
+++ Задание 1.py
@@ -6,9 +6,10 @@ try:
     auto_list = [[randint(0,20) for _ in range(n)] for _ in range(m)]
     print("Исходный список: ", *auto_list, sep="\n")
 
+    p=[]
     for i in range(len(auto_list)):
         if i%2!=0:
-            auto_list[i].reverse()
+            auto_list[i] = (auto_list[i]) [::-1] 
     print("Выходной список: ", *auto_list, sep = "\n")
 
 except:
