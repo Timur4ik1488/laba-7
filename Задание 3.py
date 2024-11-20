85632f9633180e86d1b413677e64469f9b590e5b
Timur <kirichenkotimur@gmail.com>
2024-11-20 22:44:40 +0300

    Конечное задание 3
    Конечный вариант с проверкой вводимых данных, прописано всё через 2 функции, работает исправно, но надо будет допилить

---
+++ Задание 3.py
@@ -0,0 +1,35 @@
+try:
+    n=int(input("Введите количество строк: "))
+    country_city = [input()]
+
+    def f(n, country_city):
+        dict = {}
+        for line in country_city:
+            parts = line.split()
+            country = parts[0]
+            cities = ''.join(parts[1:]).split(',')
+            dict[country] = [city.strip() for city in cities]
+        return dict
+
+    def g(dict, city):
+        for country, cities in dict.items():
+            if city in cities:
+                return(country)
+        return ""
+
+    dict = f(n,country_city)
+    city_search = input("Введите город, который необходимо найти: ")
+    country = g(dict, city_search)
+    if country:
+        print(f"Город {city_search} находится в стране: {country}")
+    else:
+        print("")
+except:
+    print("Неверно введено значение для количества строк")
+
+
+
+
+
+
+
